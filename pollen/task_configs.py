32859c353e695d34016dee7f094d6a411182146d
apirrone <awanegenebayo@gmail.com>
2024-01-08 13:35:39 +0100

    update

--- pollen/task_configs.py
+++ pollen/task_configs.py
@@ -1,13 +1,13 @@
-DATA_DIR = "/home/antoine/Pollen/act-plus-plus/pollen/episodes/"
+DATA_DIR = "/home/apirrone/Pollen/act-plus-plus/pollen/episodes/"
 TASK_CONFIGS = {
     "pollen_r_yaw_sin": {
         "dataset_dir": DATA_DIR + "/pollen_r_yaw_sin/",
         "episode_len": 500,
         "camera_names": ["cam_head"],
     },
-    "pollen_grab_bottle": {
-        "dataset_dir": DATA_DIR + "/pollen_grab_bottle/",
-        "episode_len": 500,
+    "pollen_grab_cube2": {
+        "dataset_dir": DATA_DIR + "/pollen_grab_cube2/",
+        "episode_len": 300,
         "camera_names": ["cam_head"],
     },
 }
